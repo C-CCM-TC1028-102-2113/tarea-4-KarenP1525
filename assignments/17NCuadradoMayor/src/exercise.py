1754cbb1626693812e4ef0ca6c3762cf1893d0c6
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-09-07 23:05:08 -0500

    Update exercise.py
    elevadoMayor

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,14 @@
-
-
 def main():
-    #Escribe tu código debajo de esta línea
+    import math
+    num = int(input("Escribe un numero : "))
+    elevado=0
+    i=0
+    while (elevado<=num):
+        i=i+1
+        elevado=math.pow(i,2)
+    print(int(math.sqrt(elevado)))
+
     pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
