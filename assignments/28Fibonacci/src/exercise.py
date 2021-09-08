3363c24bdb0fa713217ca41405a34563142496f3
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-09-08 00:08:34 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,17 @@
-
 def main():
-    #escribe tu código abajo de esta línea
+    index = int(input("Enter the index: "))
+    n1=0
+    i=1
+    n2=1
+    serie=0
+    while (i<index):
+        i=i+1
+        serie=n1+n2
+        n1=n2
+        n2=serie
+    print(serie)
+
     pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
